939fed526aed956087fac3c5d13b628604fe9400
Ark21bit <90682332+Ark21bit@users.noreply.github.com>
2022-10-17 09:19:38 +0000

    слайдер на гридах

---
+++ js.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters        
+  
+    // If we need pagination
+    
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    
+  });
